f7279b77c036f85cbfd6847bb9135c309573edb9
Marton Gyiran <mayer62@freemail.hu>
2020-09-12 14:06:42 +0200

    init

---
+++ HelloWord/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Vezetéknév: " + args[0]);
+		System.out.println("Keresztnév: " + args[1]);
+		System.out.println("Életkor: " + args[2]);
+				  
+	}
+
+}
